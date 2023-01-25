59f62e3f1ae3ea878ace0bff9c3dd01ef95a15af
Antonio Morales <tonitonimorales@gmail.com>
2023-01-25 14:25:35 +0100

    NEW REPOSITORIO PHP

--- includes/templates/footer.php
+++
@@ -1,5 +0,0 @@
-<footer>
-    <a href="mailto:morales8auv@gmail.com">morales8auv@gmail.com</a>
-    <a href="https://instagram.com/__mproduction?igshid=YmMyMTA2M2Y=" target="_blank"><img src="<?= WEBROOT ?>assets/icons/instagram.png" alt=""></a>
-    <p>©2022 por Mproduction.</p>
-  </footer>
